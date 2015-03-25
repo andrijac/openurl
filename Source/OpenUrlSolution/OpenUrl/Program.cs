5a96c4135c8f1fa685a70cd8b6dbc55d1040acb2
andrijac <andrija11@gmail.com>
2015-03-25 22:14:01 +0400

    Implementation and binary

--- Source/OpenUrlSolution/OpenUrl/Program.cs
+++ Source/OpenUrlSolution/OpenUrl/Program.cs
@@ -1,15 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OpenUrl
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
+			#region Help
+
+			if (args.Length == 0)
+			{
+				Help();
+				DebuggerStop();
+				return;
+			}
+
+			#endregion Help
+
+			string url = args[0];
+
+			url = FixUrl(url);
+
+			try
+			{
+				System.Diagnostics.Process.Start(url);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			DebuggerStop();
+		}
+
+		private static string FixUrl(string url)
+		{
+			string[] schemes = { "http:", "https:", "ftp:", "file:" };
+
+			var query = schemes.Where(s => url.StartsWith(s));
+
+			if (query.Count() == 0)
+			{
+				return string.Concat("http://", url);
+			}
+
+			return url;
+		}
+
+		private static void DebuggerStop()
+		{
+			if (Debugger.IsAttached)
+			{
+				Console.ReadLine();
+			}
+		}
+
+		private static void Help()
+		{
+			Console.WriteLine("openurl [url]");
 		}
 	}
 }
